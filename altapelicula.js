c6342c23b7b7ac589989fbde45e5b2e767ffb0fc
FrancoColavella <francocolavella1@gmail.com>
2021-07-07 20:29:08 -0300

    hola

--- altapelicula.js
+++ altapelicula.js
@@ -6,28 +6,29 @@ var servidor = "https://backendpruebaedi.herokuapp.com/peliculas/alta";
 function $(demo){
     return document.getElementById(demo);
 }
-function load() {
-    
 
+function load() {
     document.getElementById("guardar").addEventListener("click", click);
- 
      
- }
+}
  function click(){
-    enviarMensajeAlServidorPost(servidor,retornoDelClick);
- 
+    enviarMensajeAlServidorPost(servidor, retornoDelClick);
  }
  function retornoDelClick(respuesta){
-    
-        alert(respuesta);
-    
+        alert(respuesta);  
 }
  
- function enviarMensajeAlServidorPost(servidor, funcionARealizar){
+function enviarMensajeAlServidorPost(servidor, funcionARealizar){
  
      var xmlhttp = new XMLHttpRequest();
      var obje = new FormData();
-
+     obje.append("titulo",$("titulo").value);
+     obje.append("anio",$("anio").value);
+     obje.append("duracion",$("duracion").value);
+     obje.append("puntaje",$("puntaje").value);
+     obje.append("imagen",$("imagen").value);
+     obje.append("trailer",$("trailer").value);
+     obje.append("descripcion",$("descripcion").value);
     
      var msg="falta ingresar datos en \n";
      var ok=true;
@@ -65,79 +66,22 @@ function load() {
 
      }
      else{
-     
-    
-     xmlhttp.open("POST", servidor, false);
-     xmlhttp.onreadystatechange = function () {
-         //Veo si llego la respuesta del servidor
-         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-             //Reviso si la respuesta es correcta
-             if (xmlhttp.status == 200) {
-                 funcionARealizar(xmlhttp.responseText);
- 
-                 
-             }
-             else {
-                 alert("ocurrio un error");
-             }
-         }
-        }
+        xmlhttp.open("POST", servidor, true);
+        xmlhttp.onreadystatechange = function () {
+            //Veo si llego la respuesta del servidor
+                                        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                                            //Reviso si la respuesta es correcta
+                                            if (xmlhttp.status == 200) {
+                                                funcionARealizar(xmlhttp.responseText);
+                                            }
+                                            else {
+                                                alert("ocurrio un error");
+                                            }
+                                        }
+                                     }
         xmlhttp.setRequestHeader("enctype", "multipart/form-data");
-
         //envio el mensaje    
         xmlhttp.send(obje);
     }
- 
- 
- 
-     }
-    
-      
-     
- 
- 
- 
 
-function Numeros(string){
-    var out = '';
-    ok=true;
-    var filtro = '1234567890';
-    for (var i=0; i<4; i++)
-       if (filtro.indexOf(string.charAt(i)) != -1)
-	     out += string.charAt(i);
-    
-         return out;
-}
-function filterFloat(evt,input){
-        var key = window.Event ? evt.which : evt.keyCode;    
-        var chark = String.fromCharCode(key);
-        var tempValue = input.value+chark;
-        if(key >= 48 && key <= 57){
-            if(filter(tempValue)=== false){
-                return false;
-            }else{       
-                return true;
-            }
-        }else{
-              if(key == 8 || key == 13 || key == 0) {     
-                  return true;              
-              }else if(key == 46){
-                    if(filter(tempValue)=== false){
-                        return false;
-                    }else{       
-                        return true;
-                    }
-              }else{
-                  return false;
-              }
-        }
-}
-function filter(__val__){
-    var preg = /^([0-9]+\.?[0-9]{0,2})$/; 
-    if(preg.test(__val__) === true){
-        return true;
-    }else{
-        return false;
-    }
-    
 }
