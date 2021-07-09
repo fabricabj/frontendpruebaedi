34c71d8f502f1df751143545db4486f538c9e870
fabricabj <colavella23@gmail.com>
2021-07-09 04:05:46 -0300

    hola

--- peliculas.js
+++ peliculas.js
@@ -83,7 +83,27 @@ function formMod(id){
 
     function retornoDelClick(respuesta){
         $('peliculas').hidden=true;
-        alert(respuesta);
+        var peliculas = JSON.parse(respuesta);
+    var todo=[];
+    peliculas.forEach(element => {
+        todo.push('<div class="col-md-3"  style="padding:1%;">'+
+                      '<div class="card" style="width: 12.5rem;background:#212121;color:white">'+
+                        '<img src="'+element.imagen+'" class="card-img-top">'+
+                        '<p><i class="fas fa-star"></i>'+element.puntaje+'</p>'+
+                        '<div class="card-body" style="height:180px">'+
+                           '<p align="center" class="card-text">'+element.titulo+'</p>'+
+                           '<input class="form-control" type="text" id="id_pelicula" name="id_pelicula" value="'+element.id_pelicula+'" hidden>'+
+                           '<button type="submit" style="float: left;margin: 5px;border-radius:30px"  id="modificar" class="btn btn-dark" onclick="formMod('+element.id_pelicula+')"><i class="fas fa-pencil-alt"></i></a>'+
+                           '<button type="submit" style="text-decoration:underline;cursor:pointer; float: left;margin-right:5px;border-radius:30px;margin-top: 2%" class="btn btn-light card-text" onclick="eliminarDato('+element.id_pelicula+')"><i class="fas fa-trash-alt"></i></button>'+
+                        '</div>'+
+                      '</div>'+
+                  '</div>'
+
+                 );
+                 
+ 
+    });
+    $('peliculas').innerHTML=todo;
     }
 
     function enviarMensajeAlServidorPost(serviFormMod, funcionARealizar){
