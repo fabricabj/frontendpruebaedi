56e6151c8cc8070d78193b2333674ec62780bb90
fabricabj <colavella23@gmail.com>
2021-07-09 02:50:03 -0300

    hola

--- peliculas.js
+++ peliculas.js
@@ -2,6 +2,7 @@ addEventListener("load", load);
 //llamo al servidor
 var servidor = "https://backendpruebaedi.herokuapp.com/peliculas";
 var serviDelete = "https://backendpruebaedi.herokuapp.com/eliminarpelicula";
+serviFormMod = "https://backendpruebaedi.herokuapp.com/FormModPelicula";
 
 function $(demo){
     return document.getElementById(demo);
@@ -26,7 +27,7 @@ function cargarPeliculas(valor) {
                         '<div class="card-body" style="height:180px">'+
                            '<p align="center" class="card-text">'+element.titulo+'</p>'+
                            '<input class="form-control" type="text" id="id_pelicula" name="id_pelicula" value="'+element.id_pelicula+'" hidden>'+
-                           '<a style="float: left;margin: 5px;border-radius:30px" href="modificarpeliculas.html" id="modificar" class="btn btn-dark"><i class="fas fa-pencil-alt"></i></a>'+
+                           '<button type="submit" style="float: left;margin: 5px;border-radius:30px"  id="modificar" class="btn btn-dark" onclick="formMod('+element.id_pelicula+')"><i class="fas fa-pencil-alt"></i></a>'+
                            '<button type="submit" style="text-decoration:underline;cursor:pointer; float: left;margin-right:5px;border-radius:30px;margin-top: 2%" class="btn btn-light card-text" onclick="eliminarDato('+element.id_pelicula+')"><i class="fas fa-trash-alt"></i></button>'+
                         '</div>'+
                       '</div>'+
@@ -75,6 +76,37 @@ function eliminarDato(id){
         }
         
 }
+function formMod(id){
+
+        var xmlhttp = new XMLHttpRequest();
+        var datos = new FormData();
+        datos.append("id_pelicula",id);
+    
+        // indico hacia donde va el mensaje
+        xmlhttp.open("GET", serviFormMod, true);
+        //seteo el evento
+        xmlhttp.onreadystatechange = function () {
+            //Veo si llego la respuesta del servidor
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                //Reviso si la respuesta es correcta
+                if (xmlhttp.status == 200) {
+                    alert("eliminado");
+                }
+                else {
+                    alert("ocurrio un error");
+                }
+            }
+        
+        }
+    
+       
+        xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+    
+        //envio el mensaje    
+        xmlhttp.send(datos);
+}
+        
+
 
 
 
