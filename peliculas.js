abc27fdff6a68ad1e1c23fd994795e15ccc6febd
fabricabj <colavella23@gmail.com>
2021-07-09 04:23:12 -0300

    hola

--- peliculas.js
+++ peliculas.js
@@ -86,19 +86,43 @@ function formMod(id){
         var peliculas = JSON.parse(respuesta);
         var todo=[];
         peliculas.forEach(element => {
-        todo.push('<div class="col-md-3"  style="padding:1%;">'+
-                      '<div class="card" style="width: 12.5rem;background:#212121;color:white">'+
-                        '<img src="'+element.imagen+'" class="card-img-top">'+
-                        '<p><i class="fas fa-star"></i>'+element.puntaje+'</p>'+
-                        '<div class="card-body" style="height:180px">'+
-                           '<p align="center" class="card-text">'+element.titulo+'</p>'+
-                           '<input class="form-control" type="text" id="id_pelicula" name="id_pelicula" value="'+element.id_pelicula+'" hidden>'+
-                           '<button type="submit" style="float: left;margin: 5px;border-radius:30px"  id="modificar" class="btn btn-dark" onclick="formMod('+element.id_pelicula+')"><i class="fas fa-pencil-alt"></i></a>'+
-                           '<button type="submit" style="text-decoration:underline;cursor:pointer; float: left;margin-right:5px;border-radius:30px;margin-top: 2%" class="btn btn-light card-text" onclick="eliminarDato('+element.id_pelicula+')"><i class="fas fa-trash-alt"></i></button>'+
+        todo.push('<div class="col-md-12">'+
+                        '<div class="form-row">'+
+                            '<div class="form-group col-md-8">'+
+                                '<label for="inputEmail4">Titulo</label>'+
+                                '<input type="text" class="form-control" name="titulo" id="titulo" value="'+element.titulo+'">'+
+                             '</div>'+
+                             '<div class="form-group col-md-4">'+
+                                 '<label for="inputPassword4">año</label>'+
+                                 '<input type="text" class="form-control" name="anio" id="anio" value="'+element.anio+'">'+
+                             '</div>'+
                         '</div>'+
-                      '</div>'+
-                  '</div>'
-
+                        '<div class="form-row">'+
+                            '<div class="form-group col-md-8">'+
+                                '<label for="inputPassword4">Duracion</label>'+
+                                '<input type="text" class="form-control" name="duracion" id="duracion" value="'+element.duracion+'">'+
+                            '</div>'+
+                            '<div class="form-group col-md-4">'+
+                                '<label for="inputPassword4">Puntaje</label>'+
+                                '<input type="text" class="form-control" name="puntaje" id="puntaje" value="'+element.puntaje+'">'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                            '<label for="inputEmail4">imagen</label>'+
+                            '<input type="text" class="form-control" name="imagen" id="imagen" value="'+element.imagen+'">'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                            '<label for="inputEmail4">trailer</label>'+
+                            '<input type="text" class="form-control" name="trailer" id="trailer" value="'+element.trailer+'">'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                            '<label for="inputEmail4">Descripcion</label>'+
+                            '<textarea type="text" class="form-control" name="descripcion" id="descripcion" value="'+element.descripcion+'"></textarea>'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                           '<button type="submit" name="modificar" id="modificar" class="btn btn-primary">Modificar</button>'+
+                        '</div>'+
+                '</div>'
                  );
                  
  
